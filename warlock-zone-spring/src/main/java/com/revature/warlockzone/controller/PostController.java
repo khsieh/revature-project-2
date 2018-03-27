2a744bd0be63e80848529ec41572e641b47230de
Gabriel Kigundu <gabrielkigundu@gmail.com>
2018-03-27 12:30:07 -0400

    added logging to controllers

--- warlock-zone-spring/src/main/java/com/revature/warlockzone/controller/PostController.java
+++ warlock-zone-spring/src/main/java/com/revature/warlockzone/controller/PostController.java
@@ -2,6 +2,7 @@ package com.revature.warlockzone.controller;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,49 +19,57 @@ import com.revature.warlockzone.services.PostService;
 //allows communication from other ip's
 @CrossOrigin
 public class PostController {
-
+	private static Logger log = Logger.getLogger(PostController.class.getName());
 	@Autowired
 	private PostService postService;
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/post")
 	public void addPost(@RequestBody Post post) {
+		log.info("addPost: " + post.toString());
 		postService.addPost(post);
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/post/RecentPosts")
 	public List<Post> addAndRetrieve(@RequestBody Post post) {
+		log.info("addAndRetrieve: " + post.toString());
 		postService.addPost(post);
 		return postService.getLastTenPosts();
 	}
 	
 	@RequestMapping("/post")
 	public List<Post> getPosts(){
+		log.info("getPosts");
 		return postService.getAllPosts();
 	}
 	
 	
 	@RequestMapping("/post/{id}")
 	public Post getPost(@PathVariable int id){
+		log.info("getPost: "+ id);		
 		return postService.getPostById(id);
 	}
 	
 	@RequestMapping("/RecentPosts")
 	public List<Post> getRecentPosts(){
+		log.info("getRecentPosts");
 		return postService.getLastTenPosts();
 	}
 	
 	@RequestMapping("/RecentPosts/{id}")
 	public List<Post> getRecentPosts(@PathVariable int id){
+		log.info("getRecentPosts: "+ id);
 		return postService.getNextTenPosts(id);
 	}
 	
 	@RequestMapping(method = RequestMethod.PUT, value = "/post")
 	public void updatePost(@RequestBody Post post) {
+		log.info("updatePost");
 		postService.updatePost(post);
 	}
 	
 	@RequestMapping(method = RequestMethod.DELETE, value = "/post/{id}")
 	public void deletePost(@PathVariable int id) {
+		log.warn("deletePost: " + id);
 		postService.deletePost(id);
 	}
 	
--- warlock-zone-spring/src/main/java/com/revature/warlockzone/controller/UserController.java
+++ warlock-zone-spring/src/main/java/com/revature/warlockzone/controller/UserController.java
@@ -2,6 +2,7 @@ package com.revature.warlockzone.controller;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,17 +17,19 @@ import com.revature.warlockzone.services.UserService;
 @RestController
 @CrossOrigin
 public class UserController {
-	
+	private static Logger log = Logger.getLogger(UserController.class.getName());
 	@Autowired
 	UserService userService;
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/user")
 	public void addUser(@RequestBody User user) {
+		log.info("addUser " + user.toString());
 		userService.addUser(user);
 	}
 	
 	@RequestMapping("/user")
 	public List<User> getUsers() {
+		log.info("getAllUsers");
 		return userService.getAllUsers();
 	}
 	
@@ -37,16 +40,19 @@ public class UserController {
 	
 	@RequestMapping("/user/{username}")
 	public User getUserByUsername(@PathVariable String username) {
+		log.info("getUserByUsername: " + username);
 		return userService.getUserByUsername(username);
 	}
 	
 	@RequestMapping(method = RequestMethod.PUT, value = "/user")
 	public void updateUser(@RequestBody User user) {
+		log.info("updateUser: " + user.toString());
 		userService.updateUser(user);
 	}
 	
 	@RequestMapping(method = RequestMethod.DELETE, value = "/user/{id}")
 	public void deleteUserById(@PathVariable int id) {
+		log.warn("deleteUser: " + id);
 		userService.deleteUserById(id);
 	}
 }
