957d98a5375f9c04617b553ff83917377c66b45f
sgm4789 <sgm4789@gmail.com>
2018-03-27 20:13:28 -0400

    added feature to get all of users posts, fixed bug dentrimental to security

--- warlock-zone-spring/src/main/java/com/revature/warlockzone/dao/PostDAO.java
+++ warlock-zone-spring/src/main/java/com/revature/warlockzone/dao/PostDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.revature.warlockzone.beans.Post;
+import com.revature.warlockzone.beans.User;
 
 public interface PostDAO extends JpaRepository<Post, Integer>{
 	
@@ -13,6 +14,8 @@ public interface PostDAO extends JpaRepository<Post, Integer>{
 	public List<Post> findFirst10ByOrderByPostIdDesc();
 
 	public List<Post> findFirst10ByPostIdLessThanOrderByPostIdDesc(int id);
+	
+	public List<Post> findAllByUser(User user);
 
 	
 }
