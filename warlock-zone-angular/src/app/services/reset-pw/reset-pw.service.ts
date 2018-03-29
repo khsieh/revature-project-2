1476801d394076f1338b60d8bf214ea3d5e2f89f
Kevin Hsieh <kevin.b.hsieh@gmail.com>
2018-03-29 02:24:50 -0400

    changes to edit user forms

--- warlock-zone-angular/src/app/services/reset-pw/reset-pw.service.ts
+++ warlock-zone-angular/src/app/services/reset-pw/reset-pw.service.ts
@@ -1,13 +1,38 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class ResetPwService {
 
+    private urlEmail:string  = "http://ec2-13-58-228-189.us-east-2.compute.amazonaws.com/forgot-password";
+    private urlRPW:string  = "http://ec2-13-58-228-189.us-east-2.compute.amazonaws.com/reset-password";
+
     constructor(private httpClient: HttpClient) { }
 
-    requestEmail() {
-        //   this.httpClient.
+    requestEmail(email:string) {
+        const requestBody = email;
+        const header = {
+            headers:new HttpHeaders({
+                ContentType:'text',
+                responseType:'text',
+                observe:'response'
+            })
+        }
+
+        return this.httpClient.post(this.urlEmail,requestBody,header);
+    }
+
+    resetPW(newPassword:string, token:string ){
+        const requestBody = newPassword;
+        const header = {
+            headers:new HttpHeaders({
+                ContentType:'text',
+                responseType:'text',
+                observe:'response',
+            })
+        }
+        this.urlRPW += "?token=" + token;
+        return this.httpClient.post(this.urlRPW,requestBody,header);
     }
 
 }
--- warlock-zone-spring/src/main/java/com/revature/warlockzone/services/UserService.java
+++ warlock-zone-spring/src/main/java/com/revature/warlockzone/services/UserService.java
@@ -13,67 +13,68 @@ import com.revature.warlockzone.dao.UserDAO;
 @Service
 public class UserService {
 
-	@Autowired
-	UserDAO userDao;
-	@Autowired
-	User user;
-	
-public User authenticate(String username, String password) {
-		
-		user = getUserByUsername(username);
-		
-		if(user.getPassword().equals(password)) {
-			return user;
-		}
-		else {
-			//user and pass are not equal
-			return null;
-		}
-	}
-	
-	BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-	public List<User> getAllUsers(){
-		//probably need to change this
-		return userDao.findAll();	
-	}
-	
-	public User getUser(int id) {
-		Optional search = userDao.findById(id);
-		return (search.isPresent()) ? (User) search.get() : null;
-	}
-	
-	public User getUserByUsername(String username){
-		//probably need to change this
-		user = userDao.findByUsername(username);
-		return user;
-	}
-	
-	public void addUser(User user) {
-		String hashedPassword = passwordEncoder.encode(user.getPassword());
-		user.setPassword(hashedPassword);
+    @Autowired
+    UserDAO userDao;
+    @Autowired
+    User user;
 
-		userDao.save(user);
-	}
-	
-	public void updateUser(User user) {
-		String hashedPassword = passwordEncoder.encode(user.getPassword());
-		user.setPassword(hashedPassword);
-		userDao.save(user);
-	}
-	
-	public void deleteUserById(int id) {
-		userDao.deleteById(id);
-	}
-	public void updatePassword(User user) {
-		userDao.save(user);
-	}
-	
-	public void updateToken(User user) {
-		userDao.save(user);
-	}
-	public void deleteToken(int id) {
-		userDao.save(user);
-	}
-	
-	
+    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public User authenticate(String username, String password) {
+
+        user = getUserByUsername(username);
+        if(passwordEncoder.matches(password, user.getPassword())){
+        // if (user.getPassword().equals(password)) {
+            return user;
+        } else {
+            //user and pass are not equal
+            return null;
+        }
+    }
+
+    public List<User> getAllUsers() {
+        //probably need to change this
+        return userDao.findAll();
+    }
+
+    public User getUser(int id) {
+        Optional search = userDao.findById(id);
+        return (search.isPresent()) ? (User) search.get() : null;
+    }
+
+    public User getUserByUsername(String username) {
+        //probably need to change this
+        user = userDao.findByUsername(username);
+        return user;
+    }
+
+    public void addUser(User user) {
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
+
+        userDao.save(user);
+    }
+
+    public void updateUser(User user) {
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
+        userDao.save(user);
+    }
+
+    public void deleteUserById(int id) {
+        userDao.deleteById(id);
+    }
+
+    public void updatePassword(User user) {
+        userDao.save(user);
+    }
+
+    public void updateToken(User user) {
+        userDao.save(user);
+    }
+
+    public void deleteToken(int id) {
+        userDao.save(user);
+    }
+
 }
